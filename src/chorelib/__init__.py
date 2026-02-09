974ebbcc261364bdb31cbfb0247b1aff649ee061
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2026-02-10 08:41:17 +0900

    bump version

--- src/chorelib/__init__.py
+++ src/chorelib/__init__.py
@@ -20,7 +20,7 @@ from .deprunner import schedule
 from .ruledef import RuleSet
 from .utils import command, message, shell
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # Default RuleSet instance used by the module-level decorators.
 # Users can use `@chorelib.rule(...)` and `@chorelib.task` directly
